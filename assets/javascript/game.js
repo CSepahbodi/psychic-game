2bc2d0913902b77b4d0a422425b2c0e00ba93b8a
Cyrus Sepahbodi <cyrussepahbodi@yahoo.com>
2018-06-11 20:07:09 -0700

    this commit includes the variables for the game

---
+++ assets/javascript/game.js
@@ -0,0 +1,11 @@
+//variable for computer's choices
+var computerChoices = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+
+//variables for wins, losses, guesses, and guesses left
+var wins = 0;
+var losses = 0;
+var guesses = 9;
+var guessesLeft = 9;
+
+//variable for guessed letters
+var guessedLetters = [];
